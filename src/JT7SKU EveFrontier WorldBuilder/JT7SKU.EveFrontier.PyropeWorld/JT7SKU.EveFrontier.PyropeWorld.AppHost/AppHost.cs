a073fd6e2ae62b6af9059f3d731747cfcbb762dc
Janisku7 <janisku7@gmail.com>
2025-08-21 16:38:44 +0300

    adding things

---
+++ src/JT7SKU EveFrontier WorldBuilder/JT7SKU.EveFrontier.PyropeWorld/JT7SKU.EveFrontier.PyropeWorld.AppHost/AppHost.cs
@@ -0,0 +1,12 @@
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        var builder = DistributedApplication.CreateBuilder(args);
+
+
+
+
+        builder.Build().Run();
+    }
+}
