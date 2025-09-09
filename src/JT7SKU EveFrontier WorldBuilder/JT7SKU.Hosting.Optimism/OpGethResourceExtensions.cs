b47d7c8d092cc0487f2919a35f2f5c66df5346cf
Janisku7 <janisku7@gmail.com>
2025-09-09 03:54:10 +0300

    setting things up for optimism

---
+++ src/JT7SKU EveFrontier WorldBuilder/JT7SKU.Hosting.Optimism/OpGethResourceExtensions.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JT7SKU.Hosting.Optimism
+{
+    internal class OpGethResourceExtensions
+    {
+    }
+}
---
+++ src/JT7SKU EveFrontier WorldBuilder/JT7SKU.Hosting.Optimism/OptimismComputeEnvironmentResourceExtensions.cs
@@ -0,0 +1,23 @@
+﻿using Aspire.Hosting.ApplicationModel;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aspire.Hosting
+{
+    [Experimental("ASPIRECOMPUTE001")]
+    public static class OptimismComputeEnvironmentResourceExtensions
+    {
+        public static IResourceBuilder<OptimismComputeEnvironmentResource> AddComputeEnvironment(
+        this IDistributedApplicationBuilder builder,
+        [ResourceName] string name)
+        {
+            var resource = new OptimismComputeEnvironmentResource(name);
+
+            return builder.AddResource(resource);
+        }
+    }
+}
